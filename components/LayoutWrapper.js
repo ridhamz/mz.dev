cdb50ac2065a9c64e6593d85c85a9fd3e34e032f
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-11-02 14:46:13 +0100

    first step

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo-img.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -16,7 +16,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <img src={siteMetadata.siteLogo} width={50} />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Ridha Mezrigui',
-  headerTitle: 'ridhamz.dev',
-  description: 'A blog created with Next.js and Tailwind.css',
+  headerTitle: 'mz.dev',
+  description: 'The most you learn, the most you feel starving to learn more',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.jpg',
+  siteLogo: '/static/images/logo-img.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'ridhamezrigui07@gmail.com',
